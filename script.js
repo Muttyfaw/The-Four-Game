28e7bd4742cdf2f306b600be2e1ff6ac98a93f1c
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-06-14 16:27:47 +0000

    Done

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+$('button').click(function(){
+    $(this).css("background-color", "red")
+    console.log("Hmmm")
+})
+
+
+// let player1 = prompt("Enter your name player 1, your color will be blue:")
+// let player1Color = "blue"
+
+// let player2 = prompt("Enter your name player 2, your color will be red:")
+// let player2Color = "red"
